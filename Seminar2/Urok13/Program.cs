703383c1adf58cb4a00c051ad50db026a9eaf2c8
Igor <k566yo@mail.ru>
2022-08-10 22:25:21 +0500

    Добавили урок 15

---
+++ Seminar2/Urok13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+int number3 = (num / 100) % 10;
+Console.WriteLine($"Третья цифра числа {num} -> {number3}");
---
+++ Seminar2/Urok15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.Write("Введите число (1-7) дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if ((num > 7) || (num <1 ))
+{
+    Console.WriteLine("Неверный день недели");
+    return;
+}
+if ((num > 5))
+{
+    Console.WriteLine("Да, это выходной день");
+}
+else
+{
+    Console.WriteLine("Нет, это рабочий день");
+}
