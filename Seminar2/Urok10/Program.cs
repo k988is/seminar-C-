1b81eeb9fddb20b20f4fc8cb917eb29c1a7f629b
Igor <k566yo@mail.ru>
2022-08-10 18:49:08 +0500

    Добавили урок10

---
+++ Seminar2/Urok10/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int secondNumber = num / 10 % 10;
+Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
