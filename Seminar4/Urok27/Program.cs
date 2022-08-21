3eec1ff33f0742b478d0f7340fbe37c6e947dd5f
Igor <k566yo@mail.ru>
2022-08-21 10:24:08 +0500

    Добавили 27 урок

---
+++ Seminar4/Urok27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Суммф цифр = {GetSumNums(num)}");
+
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
