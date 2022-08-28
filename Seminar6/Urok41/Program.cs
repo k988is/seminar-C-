09ab43808a00052c0a93348ff2faa4f04a20cdc7
Igor <k566yo@mail.ru>
2022-08-28 10:27:16 +0500

    Добавили 41, 43  урок.

---
+++ Seminar6/Urok41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры N чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+using System;
+using static System.Console;
+
+Console.Clear();
+
+Console.Write("Введите числа через пробел: ");
+int[] numbers = GetArrayFromString(ReadLine());
+Console.WriteLine($"Количество чисел больше 0 –> {GetCountPositiveElements(numbers)}");
+Console.WriteLine();
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);
+    }
+    return result;
+}
+
+int GetCountPositiveElements(int[] array)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if(item>0) count++;
+    }
+    return count;
+}
---
+++ Seminar6/Urok43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+using System;
+using static System.Console;
+
+Console.Clear();
+Console.Write("Введите через пробел b1, k1, b2 , k2: ");
+string[] f = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+double b1 = double.Parse(f[0]);
+double k1 = double.Parse(f[1]);
+double b2 = double.Parse(f[2]);
+double k2 = double.Parse(f[3]);
+
+Console.WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
+
+
+
+
+double[] GetPoint(double inB1, double inK1, double inB2, double inK2)
+{
+    double[] result = new double[2];
+    result[0] = (inB2 - inB1) / (inK1 - inK2);
+    result[1] = inK1 * result[0] + inB1;
+    return result;
+}
